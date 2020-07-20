0a0e2576a0496f8b6e356a29b2712060d318ac4f
edwinapp89 <ewinpz@gmail.com>
2020-07-19 20:35:15 -0500

    se aprendio hacer comentarios en php

--- aprendiendo PHP/01-hola mundo/index.php.php
+++ aprendiendo PHP/01-hola mundo/index.php.php
@@ -1,8 +1,7 @@
-<?php
 
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 
+<h1> hola mundos esto es HTML</h1> 
+
+<?php 
+    echo' <h1>hola mundo esto es PHP</h1> ';
+?>
---
+++ aprendiendo PHP/02-imprimir/index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE HTML>
+<html lang="es">
+     <head>
+         <meta charset="utf-8"/>
+         <title>imprimir por pantalla - MASTER EN PHP <?php echo 'por Edwin Perez'; ?></title>
+     </head>
+     <body>
+         <h1>Master en PHP</h1>
+         
+         <!-- atajo para usar php, abre un  !!!Echo importante!!!!-->
+         <?=" Bienvenido Don Edwin"; ?>
+         <?php
+         // comentario en php
+         /* Titular de la seccion */
+         
+         
+            echo '<h3>Listado de video Juegos</h3>';
+            echo "<ui>"
+                 ."<li>Watch Dogs </li>"
+                 ."<li>FIFA</li>"
+                 ."<li>Prince of Persia</li>"
+                 ."<ul>";
+            echo '<p> esta es toda'.' - '.'la lista de juegos'.'</p>';
+         ?>
+     </body>
+ </html>
+
+
