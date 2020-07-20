11de533f3210a700f175f5779b6cb2866b726d25
edwinapp89 <ewinpz@gmail.com>
2020-07-19 22:32:37 -0500

    se aprendio a crear variables en php

---
+++ aprendiendo PHP/03-crear variables/index.php
@@ -0,0 +1,14 @@
+<?php
+// UNA VARIABLE ES UN CONTENEDOR DE INFORMACION O DATOS
+    $mi_primera_variable='hola mundo desde una variable';
+   
+    $verdadero=true;
+  
+    echo '<h1>' . $mi_primera_variable . '</h1>' . '<h4>' . $verdadero . '</h4>';
+    $numero = 7;
+    echo $numero;
+    $numero+=44;
+    echo '<h3>' . $numero . '</h3>';
+    $numero+=120;
+    echo $numero;
+?>
